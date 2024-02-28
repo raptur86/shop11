ec7501fb852638b3bf5b254d21611b027202c3a2
kyerim1 <nilzero25@gmail.com>
2024-02-29 07:32:30 +0900

    29일 새벽

---
+++ src/main/java/com/weapon/shop/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.weapon.shop.repository;
+import com.weapon.shop.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByMemberId(Long memberId);
+
+}
